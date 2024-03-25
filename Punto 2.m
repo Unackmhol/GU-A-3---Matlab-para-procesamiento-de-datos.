516c39ae19c2f18a945bea2c7e8b416c88cf88fc
Sulibeth <sulibethrincon.r@academia.umb.edu.co>
2024-03-25 03:52:35 +0000

    Correccion para que todas la graficas se mostraran de una vez. asignarle nombre a cada una.

--- Punto 2.m
+++ Punto 2.m
@@ -10,26 +10,31 @@ y4 = exp(x);
 y5 = log10(x2);
 
 %Representación gráfica de las funciones
+figure(1);
 plot(x,y1, 'Color',[34,90,245]/255)
 xlabel('X')
 ylabel('Y')
 title('|X|')
 
+figure(2);
 plot(x2,y2, 'Color',[36,249,255]/255)
 xlabel('X')
 ylabel('Y')
 title('√x')
 
+figure(3);
 plot(x,y3, 'Color',[65,255,36]/255)
 xlabel('X')
 ylabel('Y')
 title('cos(x)')
 
+figure(4);
 plot(x,y4, 'Color',[255,36,255]/255)
 xlabel('X')
 ylabel('Y')
 title('e^x')
 
+figure(5);
 plot(x2,y5, 'Color',[255,161,37]/255)
 xlabel('X')
 ylabel('Y')
