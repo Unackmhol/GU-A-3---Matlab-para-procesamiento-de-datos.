53eff04b177ea751fa9c525d79b2b3417c9c02c4
Sulibeth <sulibethrincon.r@academia.umb.edu.co>
2024-03-25 02:31:58 +0000

    Punto dos completado por Sebastian Traslaviña.

---
+++ Punto 2.m
@@ -0,0 +1,36 @@
+
+%Dominio en común
+x = linspace(-10,10,40);
+x2 = linspace(0,40); %Dominio específico de la raíz y logaritmo
+%Rangos específicos de las funciones a realizar
+y1 = abs(x);
+y2 = sqrt(x2);
+y3 = cos(x);
+y4 = exp(x);
+y5 = log10(x2);
+
+%Representación gráfica de las funciones
+plot(x,y1, 'Color',[34,90,245]/255)
+xlabel('X')
+ylabel('Y')
+title('|X|')
+
+plot(x2,y2, 'Color',[36,249,255]/255)
+xlabel('X')
+ylabel('Y')
+title('√x')
+
+plot(x,y3, 'Color',[65,255,36]/255)
+xlabel('X')
+ylabel('Y')
+title('cos(x)')
+
+plot(x,y4, 'Color',[255,36,255]/255)
+xlabel('X')
+ylabel('Y')
+title('e^x')
+
+plot(x2,y5, 'Color',[255,161,37]/255)
+xlabel('X')
+ylabel('Y')
+title('log10(x)')
